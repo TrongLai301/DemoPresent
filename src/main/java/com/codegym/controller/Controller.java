cfdcd33b5b2b9efa555caab75596d848b481300e
Lại Quang Trọng <tronglai301@gmail.com>
2024-01-16 15:30:18 +0700

    update code

--- src/main/java/com/codegym/controller/Controller.java
+++ src/main/java/com/codegym/controller/Controller.java
@@ -1,10 +1,9 @@
 package com.codegym.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller
-public class LoginController {
+@org.springframework.stereotype.Controller
+public class Controller {
     @GetMapping("/international")
     public String getInternationalPage(){
         return "/international";
